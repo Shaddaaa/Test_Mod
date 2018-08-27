bf3e4c086c36b3821fa74253a445ab1eafe3bef6
Shaddaaa <florian@kaufmann.cloud>
2018-08-27 16:14:44 +0200

    Removed Obsidian Tools Fixed non transparent textures to be transparent.

--- src/main/java/flokau/testmod/init/ModItems.java
+++ src/main/java/flokau/testmod/init/ModItems.java
@@ -12,9 +12,9 @@ public class ModItems
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item OBSIDIAN_INGOT = new ItemBase("obsidian_ingot");
-	public static final Item OBSIDIAN_SWORDS = new ItemBase("obsidian_sword");
-	public static final Item OBSIDIAN_PICKAXE = new ItemBase("obsidian_pickaxe");
-	public static final Item OBSIDIAN_AXE = new ItemBase("obsidian_axe");
-	public static final Item OBSIDIAN_SHOVEL = new ItemBase("obsidian_shovel");
-	public static final Item OBSIDIAN_HOE = new ItemBase("obsidian_hoe");
+	//public static final Item OBSIDIAN_SWORDS = new ItemBase("obsidian_sword");
+	//public static final Item OBSIDIAN_PICKAXE = new ItemBase("obsidian_pickaxe");
+	//public static final Item OBSIDIAN_AXE = new ItemBase("obsidian_axe");
+	//public static final Item OBSIDIAN_SHOVEL = new ItemBase("obsidian_shovel");
+	//public static final Item OBSIDIAN_HOE = new ItemBase("obsidian_hoe");
 }
